aed084127e1571ebc93208c9005b425868fa2e3a
Elisa Rachel <elisarachel2@gmail.com>
2024-09-04 11:50:53 -0300

    fix: Corrigindo erros de digitação

--- src/main/java/com/api/portaldatransparencia/PortalDaTransparenciaApplication.java
+++ src/main/java/com/api/portaldatransparencia/PortalDaTransparenciaApplication.java
@@ -1,4 +1,4 @@
-package com.api.api;
+package com.api.portaldatransparencia;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
