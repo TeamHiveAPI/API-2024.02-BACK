3fe19dd46b77e6ab6f77d74c334e68622312b4a7
Júlia Rosado <142221388+juliamariahr@users.noreply.github.com>
2024-09-13 10:19:50 -0300

    feat: Adicionei rotas para buscar o projeto por id ou filtros

--- src/main/java/com/api/portaldatransparencia/controller/ProjetoController.java
+++ src/main/java/com/api/portaldatransparencia/controller/ProjetoController.java
@@ -1,50 +1,74 @@
 package com.api.portaldatransparencia.controller;
 
-import com.api.portaldatransparencia.model.Projeto;
-import com.api.portaldatransparencia.service.ProjetoService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.api.portaldatransparencia.model.Projeto;
+import com.api.portaldatransparencia.service.ProjetoService;
 
 @RestController
 @RequestMapping("/projetos")
 public class ProjetoController {
 
-	@Autowired
-	private ProjetoService projetoService;
+    @Autowired
+    private ProjetoService projetoService;
 
-	@PostMapping
-	public Projeto criarProjeto(@RequestBody Projeto projeto) {
-		return projetoService.salvarProjeto(projeto);
-	}
+    @PostMapping
+    public Projeto criarProjeto(@RequestBody Projeto projeto) {
+        return projetoService.salvarProjeto(projeto);
+    }
 
-	@GetMapping
-	public List<Projeto> listarProjetos() {
-		return projetoService.listarProjetos();
-	}
+    @GetMapping
+    public List<Projeto> listarProjetos() {
+        return projetoService.listarProjetos();
+    }
 
-	@GetMapping("/{id}")
-	public ResponseEntity<Projeto> buscarProjetoPorId(@PathVariable Long id) {
-		Optional<Projeto> projeto = projetoService.buscarProjetoPorId(id);
-		return projeto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
-	}
+    @GetMapping("/{id}")
+    public ResponseEntity<Projeto> buscarProjetoPorId(@PathVariable Long id) {
+        Optional<Projeto> projeto = projetoService.buscarProjetoPorId(id);
+        return projeto.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+    }
 
-	@DeleteMapping("/{id}")
-	public ResponseEntity<Void> deletarProjeto(@PathVariable Long id) {
-		projetoService.deletarProjeto(id);
-		return ResponseEntity.noContent().build();
-	}
+    @GetMapping("/search")
+    public List<Projeto> buscarProjetos(
+            @RequestParam(required = false) String referencia,
+            @RequestParam(required = false) String coordenador,
+            @RequestParam(required = false) LocalDate dataInicio,
+            @RequestParam(required = false) LocalDate dataTermino,
+            @RequestParam(required = false) String classificacao,
+            @RequestParam(required = false) String situacao
+    ) {
+        return projetoService.buscarProjetos(referencia, coordenador, dataInicio, dataTermino, classificacao, situacao);
+    }
 
-	@PutMapping("/{id}")
-	public ResponseEntity<Projeto> atualizarProjeto(@PathVariable Long id, @RequestBody Projeto projeto) {
-		Optional<Projeto> projetoExistente = projetoService.buscarProjetoPorId(id);
-		if (projetoExistente.isPresent()) {
-			projeto.setId(id);
-			return ResponseEntity.ok(projetoService.salvarProjeto(projeto));
-		}
-		return ResponseEntity.notFound().build();
-	}
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deletarProjeto(@PathVariable Long id) {
+        if (projetoService.buscarProjetoPorId(id).isPresent()) {
+            projetoService.deletarProjeto(id);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Projeto> atualizarProjeto(@PathVariable Long id, @RequestBody Projeto projeto) {
+        if (projetoService.buscarProjetoPorId(id).isPresent()) {
+            projeto.setId(id);
+            return ResponseEntity.ok(projetoService.salvarProjeto(projeto));
+        }
+        return ResponseEntity.notFound().build();
+    }
 }
